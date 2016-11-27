ca1bebc4a81181a88490e7bb15b898c543c81bd3
Chris Paiano <chris.paiano@gmail.com>
2016-11-27 11:48:38 -0800

    +Now also unmounting drive to fix journal

--- scripts/fixjournalext4.sh
+++ scripts/fixjournalext4.sh
@@ -10,6 +10,7 @@ if [[ ! $(whoami) =~ "root" ]]; then
 fi
 
 echo "Attempting to fix journal of %1... (you should pass /dev/sdX# device to this script if you did not...)"
+umount "%1"
 tune2sf -O ^has_journal "%1"
 e2fsck -f "%1"
 tune2sf -j "%1"
