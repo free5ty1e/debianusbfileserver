765f83924e6e5ce148b1ac00b3d836651ebfdb69
free5ty1e <chris.paiano@gmail.com>
2020-08-01 18:58:23 -0700

    Update syncdrive.sh
    now dry run confirm before proceed

--- scripts/syncdrive.sh
+++ scripts/syncdrive.sh
@@ -22,8 +22,15 @@ SYNCLOC2="$2"
 #sudo bash -c "for i in /sys/bus/usb/devices/*/power/autosuspend; do echo 2 > $i; done"
 #sudo bash -c "for foo in /sys/bus/usb/devices/*/power/level; do echo on > $foo; done"
 
+echo "Performing dry run first to allow review of changes before proceeding"
+
+rsync --archive --verbose --stats --whole-file --progress --times --dry-run --human-readable "${SYNCLOC1}/" "${SYNCLOC2}/"
+
+echo "Continue actually syncing if the above dry run results look correct, otherwise press CTRL-C to cancel and investigate!"
+
+read -p "Press enter to continue"
 
 echo "Syncing ${SYNCLOC1}/ with ${SYNCLOC2}/"
 
 echo "DELETIONS WILL NOT BE PROPOGATED, THIS IS ON PURPOSE TO PREVENT DATA CORRUPTION FROM PROPOGATING"
-rsync -av --stats --whole-file --progress --log-file="${SYNCLOC1}/rsync.log" "${SYNCLOC1}/" "${SYNCLOC2}/"
+rsync --archive --verbose --stats --whole-file --progress --times --human-readable --log-file="${SYNCLOC1}/rsync.log" "${SYNCLOC1}/" "${SYNCLOC2}/"
