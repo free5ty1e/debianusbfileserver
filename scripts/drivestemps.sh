3b6773b65bf29886d40f56ac674a92087cfc1d56
Chris Paiano <chris.paiano@gmail.com>
2020-08-30 15:25:01 -0700

    Further cleanup of drivestemps.sh output

--- scripts/drivestemps.sh
+++ scripts/drivestemps.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-for file in /dev/sd*
+for file in /dev/sd*1
 do
-	echo "190 Airflow_Temperature_Cel 0x0022   058   019   045    Old_age   Always   In_the_past 42 (Min/Max 27/50 #25158)"
 	echo "Reading temperature of $file ---------------------------------------------------- $file -------------"
 	drivetemp.sh "$file"
 done
