bad77842360bf6e90a65b8f3671d2b3faaaac6a1
Chris Paiano <chris.paiano@gmail.com>
2020-08-30 15:22:22 -0700

    More visually useful output for drivestempts.sh

--- scripts/drivestemps.sh
+++ scripts/drivestemps.sh
@@ -2,6 +2,7 @@
 
 for file in /dev/sd*
 do
-	echo "Reading temperature of $file"
+	echo "190 Airflow_Temperature_Cel 0x0022   058   019   045    Old_age   Always   In_the_past 42 (Min/Max 27/50 #25158)"
+	echo "Reading temperature of $file ---------------------------------------------------- $file -------------"
 	drivetemp.sh "$file"
 done
