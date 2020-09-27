5a3c9f68e87e03fbf9cc0fb273e1232ac2d56769
Chris Paiano <chris.paiano@gmail.com>
2020-09-27 15:50:28 -0700

    Disabled broken enforcement of sleep policy

--- scripts/mountusbbylabel.sh
+++ scripts/mountusbbylabel.sh
@@ -9,9 +9,9 @@ if [[ ! $(whoami) =~ "root" ]]; then
 	exit
 fi
 
-echo "Enforcing no-sleep USB policies..."
-bash -c "for i in /sys/bus/usb/devices/*/power/autosuspend; do echo 2 > $i; done"
-bash -c "for foo in /sys/bus/usb/devices/*/power/level; do echo on > $foo; done"
+# echo "Enforcing no-sleep USB policies..."
+# bash -c "for i in /sys/bus/usb/devices/*/power/autosuspend; do echo 2 > $i; done"
+# bash -c "for foo in /sys/bus/usb/devices/*/power/level; do echo on > $foo; done"
 
 echo "Automounting all USB drives by volume name / label..."
 for dev in $(ls -1 /dev/disk/by-label/* | grep -v EFI) ; do
