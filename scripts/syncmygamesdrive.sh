bc5fcb6d91deb629bd43292c86ca707bccae1ce2
Chris Paiano <chris.paiano@gmail.com>
2020-10-04 13:04:55 -0700

    Fixed syncmygamesdrive.sh

--- scripts/syncmygamesdrive.sh
+++ scripts/syncmygamesdrive.sh
@@ -3,7 +3,7 @@
 echo "Customize a copy of this script to sync your own fileserver drive names"
 
 echo "Syncing new games to playable games drive"
-syncdrive.sh /media/PrimeGamesBee/retropie-mount-bak /media/usb0/retropie-mount nodryrun
+syncdrive.sh /media/PrimeGamesBee/retropie-mount-bak /media/PRIMEGAMES/retropie-mount nodryrun
 
 echo "Syncing saved games from playable games drive to new games drive"
-syncdrive.sh /media/usb0/retropie-mount /media/PrimeGamesBee/retropie-mount-bak nodryrun
+syncdrive.sh /media/PRIMEGAMES/retropie-mount /media/PrimeGamesBee/retropie-mount-bak nodryrun
