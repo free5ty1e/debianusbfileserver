1257ced7530348b2a37707e38614bca748329c38
Chris Paiano <chris.paiano@gmail.com>
2020-10-04 13:02:49 -0700

    removed excludedPattern as it was causing issues

--- scripts/syncdrive.sh
+++ scripts/syncdrive.sh
@@ -2,9 +2,8 @@
 
 usage_notes() {
 	echo "USAGE: "
-    echo "syncdrive.sh /media/VolumeOne /media/VolumeOneBak nodryrun excludedPattern"
+    echo "syncdrive.sh /media/VolumeOne /media/VolumeOneBak nodryrun"
     echo "(nodryrun is optional and in fact can be most any word)"
-    echo "(excludedPattern is optional and only available if nodryrun is present)"
 }
 
 if [ -z "$*" ] ; then
@@ -23,23 +22,13 @@ SYNCLOC2="$2"
 #echo "Enforcing no-sleep USB policies..."
 #sudo bash -c "for i in /sys/bus/usb/devices/*/power/autosuspend; do echo 2 > $i; done"
 #sudo bash -c "for foo in /sys/bus/usb/devices/*/power/level; do echo on > $foo; done"
-if [ -z "$4" ] ; then 
-	echo "Exclude disabled"
-	SYNCEXCLUDEPARAM=""
-	SYNCEXCLUDEOPTION=""
-else
-	echo "Excluding $4"
-	SYNCEXCLUDEPARAM="--exclude"	
-	SYNCEXCLUDEOPTION="$4"
-fi
 
 if [ -z "$3" ] ; then
 	echo "Performing dry run first to allow review of changes before proceeding"
-	rsync --archive --verbose --stats --whole-file --progress --times --dry-run --one-file-system --human-readable "$SYNCEXCLUDEPARAM" "$SYNCEXCLUDEOPTION" "${SYNCLOC1}/" "${SYNCLOC2}/"
+	rsync --archive --verbose --stats --whole-file --progress --times --dry-run --one-file-system --human-readable "${SYNCLOC1}/" "${SYNCLOC2}/"
 	echo "Continue actually syncing if the above dry run results look correct, otherwise press CTRL-C to cancel and investigate!"
 	read -p "Press enter to continue"
 fi
-
 
 echo "Backing up previous rsync logfile ${SYNCLOC1}/rsync.log and deleting the previous rolling backup ${SYNCLOC1}/rsync.log.old"
 mv -vf "${SYNCLOC1}/rsync.log" "${SYNCLOC1}/rsync.log.old"
@@ -47,4 +36,4 @@ mv -vf "${SYNCLOC1}/rsync.log" "${SYNCLOC1}/rsync.log.old"
 echo "Syncing ${SYNCLOC1}/ with ${SYNCLOC2}/"
 
 echo "DELETIONS WILL NOT BE PROPOGATED, THIS IS ON PURPOSE TO PREVENT DATA CORRUPTION FROM PROPOGATING"
-rsync --archive --verbose --stats --whole-file --progress --times --one-file-system --human-readable "$SYNCEXCLUDEPARAM" "$SYNCEXCLUDEOPTION" --log-file="${SYNCLOC1}/rsync.log" "${SYNCLOC1}/" "${SYNCLOC2}/"
+rsync --archive --verbose --stats --whole-file --progress --times --one-file-system --human-readable --log-file="${SYNCLOC1}/rsync.log" "${SYNCLOC1}/" "${SYNCLOC2}/"
