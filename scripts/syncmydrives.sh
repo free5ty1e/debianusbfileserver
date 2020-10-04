66666c7929951f69c7ffb6454b67c82ce56e3604
Chris Paiano <chris.paiano@gmail.com>
2020-10-04 11:44:27 -0700

    now syncmygamesdrive.sh

--- scripts/syncmydrives.sh
+++ scripts/syncmydrives.sh
@@ -2,13 +2,9 @@
 
 echo "Customize a copy of this script to sync your own fileserver drive names"
 
+syncmygamesdrive.sh
+
 echo "Syncing data drive"
 syncdrive.sh /media/MassiveSto8TbAyy /media/MassiveSto8TbBee nodryrun
-
-echo "Syncing new games to playable games drive"
-syncdrive.sh /media/PrimeGamesBee/retropie-mount-bak /home/pi/RetroPie nodryrun
-
-echo "Syncing saved games from playable games drive to new games drive"
-syncdrive.sh /home/pi/RetroPie /media/PrimeGamesBee/retropie-mount-bak nodryrun
 
 syncmyplexdrive.sh
