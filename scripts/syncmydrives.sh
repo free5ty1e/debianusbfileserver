6fc89569ee829a773f34fd4851a3ebf59e4db4ff
Chris Paiano <chris.paiano@gmail.com>
2020-10-04 09:00:49 -0700

    Updated plex sync

--- scripts/syncmydrives.sh
+++ scripts/syncmydrives.sh
@@ -5,10 +5,10 @@ echo "Customize a copy of this script to sync your own fileserver drive names"
 echo "Syncing data drive"
 syncdrive.sh /media/MassiveSto8TbAyy /media/MassiveSto8TbBee nodryrun
 
-syncmyplexdrive.sh
-
 echo "Syncing new games to playable games drive"
 syncdrive.sh /media/PrimeGamesBee/retropie-mount-bak /home/pi/RetroPie nodryrun
 
 echo "Syncing saved games from playable games drive to new games drive"
 syncdrive.sh /home/pi/RetroPie /media/PrimeGamesBee/retropie-mount-bak nodryrun
+
+syncmyplexdrive.sh
--- scripts/syncmyplexdrive.sh
+++ scripts/syncmyplexdrive.sh
@@ -3,11 +3,11 @@
 echo "Customize a copy of this script to sync your own plex fileserver drive names"
 
 
-echo "Syncing Plex media drive and archived Plex database"
+echo "Syncing Plex media drive and Plex database"
 rm -rf /media/Media8TbBeee/plexdata
-pushd /media/Media8TbAyye
-rm plexmediaserver_plexdata_bak.7z
-7z plexdata
-mv plexdata.7z plexmediaserver_plexdata_bak.7z
-popd
+# pushd /media/Media8TbAyye
+# rm plexmediaserver_plexdata_bak.7z
+# 7z plexdata
+# mv plexdata.7z plexmediaserver_plexdata_bak.7z
+# popd
 syncdrive.sh /media/Media8TbAyye /media/Media8TbBeee nodryrun 'plexdata'
