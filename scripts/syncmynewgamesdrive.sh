db4d8044848beea550613989c241699c8b809353
Chris Paiano <chris.paiano@gmail.com>
2020-10-13 23:58:33 -0700

    Separated out syncmynewgamesdrive.sh

---
+++ scripts/syncmynewgamesdrive.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Customize a copy of this script to sync your own fileserver drive names"
+
+echo "Syncing new games to playable games drive"
+syncdrive.sh /media/PrimeGamesBee/retropie-mount-bak /media/PRIMEGAMES/retropie-mount nodryrun
