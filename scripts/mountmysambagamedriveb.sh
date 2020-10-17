34d223eb8b946904732d86031b746c563ad08045
Chris Paiano <chris.paiano@gmail.com>
2020-10-17 16:20:45 -0700

    Script to mount remote samba share, mine

---
+++ scripts/mountmysambagamedriveb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ ! $(whoami) =~ "root" ]]; then
+	echo ""
+	echo "**********************************"
+	echo "*** This should be run as root ***"
+	echo "**********************************"
+	echo ""
+	exit
+fi
+
+mkdir -vp /media/primegames
+mount -t cifs -o user=pi //192.168.100.104/sharedusb/PrimeGamesBee /media/primegames
