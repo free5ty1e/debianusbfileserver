87ae07079072f7bf8dc8593fb5f344f94ef9b99f
Chris Paiano <chris.paiano@gmail.com>
2020-11-25 13:51:58 -0800

    New linkUsbByLabel script

---
+++ scripts/linkUsbByLabel.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ ! $(whoami) =~ "root" ]]; then
+	echo ""
+	echo "**********************************"
+	echo "*** This should be run as root ***"
+	echo "**********************************"
+	echo ""
+	exit
+fi
+
+# echo "Enforcing no-sleep USB policies..."
+# bash -c "for i in /sys/bus/usb/devices/*/power/autosuspend; do echo 2 > $i; done"
+# bash -c "for foo in /sys/bus/usb/devices/*/power/level; do echo on > $foo; done"
+
+echo "If automounting USB HDDs at startup via usbmount, should probably wait via sleep in /etc/rc.local before executing this."
+
+echo "Autolinking all already-mounted USB drives by volume name / label..."
+for dev in $(ls -1 /dev/disk/by-label/* | grep -v EFI) ; do
+	label=$(basename $dev)
+	mountpoint=$(mount | grep $(readlink --canonicalize "$dev") | awk -v N=$3 '{print $3}')
+	test -e "/media/$label" || ln -sf "$mountpoint" "/media/$label"
+done
