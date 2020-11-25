58587ffc5d0e9c60760c650da2d086502646b504
Chris Paiano <chris.paiano@gmail.com>
2020-11-25 12:36:47 -0800

    Now can call install automount script from anywhere

--- scripts/installAutoMountUsbByLabelToUsbmount.sh
+++ scripts/installAutoMountUsbByLabelToUsbmount.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 echo "Enhancing usbmount to also symlink drives by label in /media/"
+
 # sudo mkdir -vp /etc/usbmount/mount.d
 # sudo mkdir -vp /etc/usbmount/umount.d
+
+pushd ~/debianusbfileserver
 sudo cp -vr etc/* /etc/
 sudo chmod +x /etc/usbmount/mount.d/*
 sudo chmod +x /etc/usbmount/umount.d/*
+popd
