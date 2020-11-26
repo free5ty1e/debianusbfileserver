0c5a2d39e06511881878854aaf8370744ebe8c3c
Chris Paiano <chris.paiano@gmail.com>
2020-11-25 20:36:51 -0800

    Refactored out syncmydatadrive.sh

--- scripts/syncmydrives.sh
+++ scripts/syncmydrives.sh
@@ -4,7 +4,6 @@ echo "Customize a copy of this script to sync your own fileserver drive names"
 
 syncmygamesdrive.sh
 
-echo "Syncing data drive"
-syncdrive.sh /media/MassiveSto8TbAyy /media/MassiveSto8TbBee nodryrun
+syncmydatadrive.sh
 
 syncmyplexdrive.sh
