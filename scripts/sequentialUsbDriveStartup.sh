fb90bdfc2276c1ab53afd0dfb9f3f52ce167d214
Chris Paiano <chris.paiano@gmail.com>
2020-11-25 17:31:08 -0800

    sequentialUsbDriveStartup.sh meant to have usbmount service disabled in /etc/usbmount/usbmount.conf

---
+++ scripts/sequentialUsbDriveStartup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ ! $(whoami) =~ "root" ]]; then
+	echo ""
+	echo "**********************************"
+	echo "*** This should be run as root ***"
+	echo "**********************************"
+	echo ""
+	exit
+fi
+
+# echo "Enforcing no-sleep USB policies..."
+# bash -c "for i in /sys/bus/usb/devices/*/power/autosuspend; do echo 2 > $i; done"
+# bash -c "for foo in /sys/bus/usb/devices/*/power/level; do echo on > $foo; done"
+
+echo "Enabling usbmount service"
+sed -i -E 's/(ENABLED=).*/\11/' /etc/usbmount/usbmount.conf
+
+echo "Automounting all USB drives by volume name / label..."
+for dev in $(ls /dev/sd?) ; do
+	touch $dev
+	sleep 5
+done
+
+echo "Disabling usbmount service"
+sed -i -E 's/(ENABLED=).*/\10/' /etc/usbmount/usbmount.conf
