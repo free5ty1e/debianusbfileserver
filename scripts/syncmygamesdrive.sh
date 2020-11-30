5dcb19dbf7d0c004b0d50c86bfaf452ae491517f
Chris Paiano <chris.paiano@gmail.com>
2020-11-29 17:19:16 -0800

    Updated my sync scripts to reflect latest drive labels

--- scripts/syncmygamesdrive.sh
+++ scripts/syncmygamesdrive.sh
@@ -2,8 +2,5 @@
 
 echo "Customize a copy of this script to sync your own fileserver drive names"
 
-echo "Syncing new games to playable games drive"
-syncmynewgamesdrive.sh
-
 echo "Syncing saved games from playable games drive to new games drive"
-syncdrive.sh /media/PRIMEGAMES/retropie-mount /media/PrimeGamesBee/retropie-mount-bak nodryrun
+syncdrive.sh /media/PrimeGamesAyy/retropie-mount /media/PrimeGamesBee/retropie-mount-bak nodryrun
--- scripts/syncmynewgamesdrive.sh
+++ scripts/syncmynewgamesdrive.sh
@@ -3,4 +3,4 @@
 echo "Customize a copy of this script to sync your own fileserver drive names"
 
 echo "Syncing new games to playable games drive"
-syncdrive.sh /media/PrimeGamesBee/retropie-mount-bak /media/PRIMEGAMES/retropie-mount nodryrun
+syncdrive.sh /media/PrimeGamesBee/retropie-mount-bak /media/PrimeGamesAyy/retropie-mount nodryrun
