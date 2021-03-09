c241562e8761e103ff64de75d8fa73496e62e658
Chris Paiano <chris.paiano@gmail.com>
2021-03-08 22:45:55 -0800

    Exporting year from filename var

--- scripts/yearfromfiletimestamp.sh
+++ scripts/yearfromfiletimestamp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Extracting year from filename $1 timestamp... (Provide filename as parameter #1)"
-YEAR_FROM_FILENAME=$(echo "$1" | grep -o -m1 '[0-9]\{4\}' | awk 'NR==1')
+export YEAR_FROM_FILENAME=$(echo "$1" | grep -o -m1 '[0-9]\{4\}' | awk 'NR==1')
 echo "Year extracted: $YEAR_FROM_FILENAME"
