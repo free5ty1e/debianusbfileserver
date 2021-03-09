42403bf41f9b3587d9e18b21eb5e640c514b1122
Chris Paiano <chris.paiano@gmail.com>
2021-03-08 22:09:57 -0800

    Corrected syntax of new scripts

--- scripts/imageshrinktohd.sh
+++ scripts/imageshrinktohd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "Resizing image %1 to HD as %2.  If needed, sudo apt-get install imagemagick"
-convert "%1" -resize 1920x1920\> "%2"
+echo "Resizing image $1 to HD as $2.  If needed, sudo apt-get install imagemagick"
+convert "$1" -resize 1920x1920\> "$2"
