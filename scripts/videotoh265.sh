0a99f0bcc8b72e6245edab92d592bb92f86dd139
Chris Paiano <chris.paiano@gmail.com>
2021-03-08 23:36:58 -0800

    Threads limited to 1 for ffmpeg transcodes

--- scripts/videotoh265.sh
+++ scripts/videotoh265.sh
@@ -2,10 +2,10 @@
 
 echo "Processing video $1 for archival transcoding to h.265 and downscaling to 720p with mp3 audio to output file $2"
 echo "(You should provide the first parameter as the filename and second as the target for output)"
+echo "If needed, sudo apt-get install cpulimit ffmpeg"
 
 #1-pass, 720p, veryslow, mp3 audio defaults
 #ffmpeg -i "$1" -vcodec libx265 -crf 28 -preset veryslow -acodec libmp3lame -vf 'scale=1280:1280:force_original_aspect_ratio=decrease' "$2"
 
-#1-pass, 720p, veryslow, audio default
-ffmpeg -i "$1" -vcodec libx265 -crf 28 -preset veryslow -vf 'scale=1280:1280:force_original_aspect_ratio=decrease' "$2"
-
+#1-pass, 720p, veryslow, audio default, threads limited to 1
+ffmpeg -threads 1 -i "$1" -vcodec libx265 -crf 28 -preset veryslow -vf 'scale=1280:1280:force_original_aspect_ratio=decrease' "$2"
