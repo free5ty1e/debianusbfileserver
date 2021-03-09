bd0441df23b3252cc4cc9bd090c30e6cc9dd394a
Chris Paiano <chris.paiano@gmail.com>
2021-03-08 22:05:50 -0800

    Created new set of jpeg image archival processing scripts to assist incoming phone camera images on syncthing

---
+++ scripts/imageoptimizejpeg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Optimizing JPEG image %1 to %2.  If needed, follow instructions here to install jpeg-recompress tool:"
+echo "https://github.com/free5ty1e/debianusbfileserver/blob/master/README.md"
+jpeg-recompress --quality medium --accurate --method smallfry --min 50 "%1" "%2"
---
+++ scripts/imageprocessforarchive.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Processing image %1 for archival as %2 - if jpeg, optimizing and shrinking.  If not jpeg, just moving."
+
+case $(file -b "%1") in
+  'JPEG '*)
+    echo "JPEG file %1 detected, proceeding with processing (optimize then shrink for best file size)..."
+    imageoptimizejpeg.sh "%1" "optimized_%1"
+    imageshrinktohd.sh "optimized_%1" "%2"
+    rm "optimized_%1"
+    ;;
+  *)
+    echo "%1 is not a JPEG file, skipping the processing..."
+    mv "%1" "%2"
+    ;;
+esac
