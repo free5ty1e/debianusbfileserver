eebd37d4b9baa55c836921e9dd93b3b99b13af48
Chris Paiano <chris.paiano@gmail.com>
2021-03-08 22:55:45 -0800

    Created basic video processing scripts to work with

---
+++ scripts/videoprocessforarchive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Processing video $1 for archival, for storage in location $2 under an appropriate year folder"
+echo "(You should provide the first parameter as the filename)"
+
+YEAR_FROM_FILENAME=$(yearfromfiletimestamp.sh "$1")
+TARGET_FOLDER="$2/$YEAR_FROM_FILENAME"
+echo "Year extracted from filename via script: $YEAR_FROM_FILENAME, creating location $TARGET_FOLDER if it does not already exist..."
+mkdir -pv "$TARGET_FOLDER"
+
+videotoh265.sh "$1" "$TARGET_FOLDER/$1"
---
+++ scripts/videotoh265.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Processing video $1 for archival transcoding to h.265 and downscaling to 720p with mp3 audio to output file $2"
+echo "(You should provide the first parameter as the filename and second as the target for output)"
+
+#1-pass, 720p, veryslow, mp3 audio defaults
+ffmpeg -i "$1" -vcodec libx265 -crf 28 -preset veryslow -acodec libmp3lame -vf 'scale=1280:1280:force_original_aspect_ratio=decrease' "$2"
