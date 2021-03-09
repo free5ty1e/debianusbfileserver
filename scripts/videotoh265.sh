80603ba044d719c78c80bd269ea0b6a426a6ee99
Chris Paiano <chris.paiano@gmail.com>
2021-03-08 23:27:18 -0800

    Updated h265 transcode script

--- scripts/videotoh265.sh
+++ scripts/videotoh265.sh
@@ -4,4 +4,8 @@ echo "Processing video $1 for archival transcoding to h.265 and downscaling to 7
 echo "(You should provide the first parameter as the filename and second as the target for output)"
 
 #1-pass, 720p, veryslow, mp3 audio defaults
-ffmpeg -i "$1" -vcodec libx265 -crf 28 -preset veryslow -acodec libmp3lame -vf 'scale=1280:1280:force_original_aspect_ratio=decrease' "$2"
+#ffmpeg -i "$1" -vcodec libx265 -crf 28 -preset veryslow -acodec libmp3lame -vf 'scale=1280:1280:force_original_aspect_ratio=decrease' "$2"
+
+#1-pass, 720p, veryslow, audio default
+ffmpeg -i "$1" -vcodec libx265 -crf 28 -preset veryslow -vf 'scale=1280:1280:force_original_aspect_ratio=decrease' "$2"
+
