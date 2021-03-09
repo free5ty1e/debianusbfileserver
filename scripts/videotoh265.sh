3ae3d44ed593cb51b78e5209f412bb6c1f58f84d
Chris Paiano <chris.paiano@gmail.com>
2021-03-09 08:04:13 -0800

    Trying 1080p for vidcomp

--- scripts/videotoh265.sh
+++ scripts/videotoh265.sh
@@ -8,4 +8,7 @@ echo "If needed, sudo apt-get install cpulimit ffmpeg"
 #ffmpeg -i "$1" -vcodec libx265 -crf 28 -preset veryslow -acodec libmp3lame -vf 'scale=1280:1280:force_original_aspect_ratio=decrease' "$2"
 
 #1-pass, 720p, veryslow, audio default
-ffmpeg -i "$1" -vcodec libx265 -crf 28 -preset veryslow -vf 'scale=1280:1280:force_original_aspect_ratio=decrease' "$2"
+#ffmpeg -i "$1" -vcodec libx265 -crf 28 -preset veryslow -vf 'scale=1280:1280:force_original_aspect_ratio=decrease' "$2"
+
+#1-pass, 1080p, veryslow, mp3 audio defaults
+ffmpeg -i "$1" -vcodec libx265 -crf 28 -preset veryslow -acodec libmp3lame -vf 'scale=1920:1920:force_original_aspect_ratio=decrease' "$2"
