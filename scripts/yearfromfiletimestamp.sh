2ba3e17c80a332275d8d16e1f5cc37cc7befe681
Chris Paiano <chris.paiano@gmail.com>
2021-03-14 00:44:28 -0800

    year stamp test

--- scripts/yearfromfiletimestamp.sh
+++ scripts/yearfromfiletimestamp.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-TIMESTAMP_YEAR=$(echo "$1" | grep -o -m1 '[0-9]\{4\}' | awk 'NR==1')
-if [ -z "$TIMESTAMP_YEAR" ] ; then
+
+regex='[0-9]{8}'
+if [[ $1 =~ $regex ]]; then
+    echo "File $1 has a timestamp in the filename, extracting year from timestamp"
+    TIMESTAMP_YEAR=$(echo "$1" | grep -o -m1 '[0-9]\{4\}' | awk 'NR==1')
+else
+    echo "File $1 does not have a timestamp in the filename, extracting year from creation date"
     TIMESTAMP_YEAR=$(stat -c %.4y "$1")
 fi
 echo "$TIMESTAMP_YEAR"
