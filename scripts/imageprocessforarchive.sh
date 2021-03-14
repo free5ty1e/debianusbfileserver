054b7d7c7463477160fffbf2d430c36ca8e3a9a5
Chris Paiano <chris.paiano@gmail.com>
2021-03-14 00:29:28 -0800

    temp optimized filename now just hardcoded

--- scripts/imageprocessforarchive.sh
+++ scripts/imageprocessforarchive.sh
@@ -3,6 +3,7 @@
 echo "Processing image $1 for archival storage in location $2 under an appropriate year folder - if jpeg, optimizing and shrinking.  If not jpeg, just moving."
 echo "(You should provide the first parameter as the image filename)"
 
+TEMP_OPTIMIZED_FILENAME="optimized_temp.jpg"
 YEAR_FROM_FILENAME=$(yearfromfiletimestamp.sh "$1")
 TARGET_FOLDER="$2/$YEAR_FROM_FILENAME"
 echo "Year extracted from filename via script: $YEAR_FROM_FILENAME, creating location $TARGET_FOLDER if it does not already exist..."
@@ -11,9 +12,9 @@ mkdir -pv "$TARGET_FOLDER"
 case $(file -b "$1") in
   'JPEG '*)
     echo "JPEG file $1 detected, proceeding with processing (optimize then shrink for best file size)..."
-    imageoptimizejpeg.sh "$1" "optimized_$1"
-    imageshrinktohd.sh "optimized_$1" "$TARGET_FOLDER/$(basename $1)"
-    rm "optimized_$1"
+    imageoptimizejpeg.sh "$1" "$TEMP_OPTIMIZED_FILENAME"
+    imageshrinktohd.sh "$TEMP_OPTIMIZED_FILENAME" "$TARGET_FOLDER/$(basename $1)"
+    rm "$TEMP_OPTIMIZED_FILENAME"
     #rm "$1"
     ;;
   *)
