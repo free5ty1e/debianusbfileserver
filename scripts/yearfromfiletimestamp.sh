e00a58ea8405c516a406926ad711967671ed165d
Chris Paiano <chris.paiano@gmail.com>
2021-03-13 23:44:12 -0800

    yearfromfiletimestamp.sh now falls back on file creation year if timestamp yields no results

--- scripts/yearfromfiletimestamp.sh
+++ scripts/yearfromfiletimestamp.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "$1" | grep -o -m1 '[0-9]\{4\}' | awk 'NR==1'
+TIMESTAMP_YEAR=$(echo "$1" | grep -o -m1 '[0-9]\{4\}' | awk 'NR==1')
+if [ -z "$TIMESTAMP_YEAR" ] ; then
+    TIMESTAMP_YEAR=$(stat -c %.4y "$1")
+fi
+echo "$TIMESTAMP_YEAR"
