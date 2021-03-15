ae7483d9f204b4c83e7afd877aec36db40b240c5
Chris Paiano <chris.paiano@gmail.com>
2021-03-14 19:55:47 -0700

    Added screen to apt packages

--- scripts/installaptpackages.sh
+++ scripts/installaptpackages.sh
@@ -8,7 +8,7 @@ sudo apt-get -yf install
 echo "Obtaining required packages / dependencies..."
 sudo apt-get purge -y p7zip
 sudo apt-get autoremove
-sudo apt-get install -y git curl samba samba-client cifs-utils ftp curlftpfs dialog ntfs-3g cowsay pv bc expect openssl parted gdisk rsync e2fsprogs smartmontools hddtemp hdparm p7zip-full usbmount
+sudo apt-get install -y git curl samba samba-client cifs-utils ftp curlftpfs dialog ntfs-3g cowsay pv bc expect openssl parted gdisk rsync e2fsprogs smartmontools hddtemp hdparm p7zip-full usbmount screen
 #joystick ntfs-3g cowsay imagemagick angband pv bc libssl-dev glib-networking expect openssl gdisk rsync
 
 # installGoAndAnsize.sh
