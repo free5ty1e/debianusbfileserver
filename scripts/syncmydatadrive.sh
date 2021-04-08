3c4c79acdfeabece060ceffac6bfa9dd2fde0d12
Chris Paiano <chris.paiano@gmail.com>
2021-04-07 22:23:12 -0700

    syncmydatadrive now excludes 2 specific folders for the sync

--- scripts/syncmydatadrive.sh
+++ scripts/syncmydatadrive.sh
@@ -3,4 +3,4 @@
 echo "Customize a copy of this script to sync your own fileserver drive names"
 
 echo "Syncing data drive"
-syncdrive.sh /media/MassiveSto8TbAyy /media/MassiveSto8TbBee nodryrun
+syncdrive.sh /media/MassiveSto8TbAyy /media/MassiveSto8TbBee nodryrun Syncthing SyncthingChrisAndroid
