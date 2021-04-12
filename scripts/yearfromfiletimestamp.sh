37fef82a79e386ab87e88c8a7531e340f74d6dbf
Chris Paiano <chris.paiano@gmail.com>
2021-04-12 14:16:46 -0700

    Experimenting with yearfromfiletimestamp

--- scripts/yearfromfiletimestamp.sh
+++ scripts/yearfromfiletimestamp.sh
@@ -3,7 +3,7 @@
 gphotosregex='Photos from [0-9]\{4\}'
 screenshotregex='_[0-9]{8}-'
 regex='_[0-9]{8}_'
-if [[ $1 =~ $regex ]]; then
+if [[ $1 =~ $gphotosregex ]]; then
     # echo "File $1 has a "Photos From XXXX" year stamp in the filename, extracting year from timestamp"
     TIMESTAMP_YEAR=$(echo "$1" | grep -o -m1 'Photos from [0-9]\{4\}' | awk 'NR==1')
 elif [[ $1 =~ $screenshotregex ]]; then
