a06138808138857deea878dda658b2baf431251c
Chris Paiano <chris.paiano@gmail.com>
2021-04-18 20:38:53 -0700

    Now can pass basefolder in to processmymediafolders script so I can process from onboard the Pi or from some remote terminal with the share mounted

--- scripts/processmymediafoldersforarchive.sh
+++ scripts/processmymediafoldersforarchive.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-BASEFOLDER="/media/MassiveSto8TbAyy"
+#If parameter 1 passed, set as BASEFOLDER.  Otherwise, use default
+if [[ $1 ]] ; then
+    echo "Parameter 1 passed for BASEFOLDER: $1"
+    BASEFOLDER="$1"
+else
+    BASEFOLDER="/media/MassiveSto8TbAyy"
+fi 
+echo "BASEFOLDER is $BASEFOLDER"
+
 processmediafolderforarchive.sh "$BASEFOLDER/SyncthingChrisAndroid/AndroidCamera" "$BASEFOLDER/SyncthingProcessed/ChrisMedia/AndroidCamera"
 processmediafolderforarchive.sh "$BASEFOLDER/SyncthingChrisAndroid/PicturesFromApps" "$BASEFOLDER/SyncthingProcessed/ChrisMedia/PicturesFromApps"
 processmediafolderforarchive.sh "$BASEFOLDER/Syncthing/CodiDox" "$BASEFOLDER/SyncthingProcessed/CodiMedia"
