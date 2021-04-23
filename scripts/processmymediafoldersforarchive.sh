cefbcf705a227168e748406840772776ba51ff49
Chris Paiano <chris.paiano@gmail.com>
2021-04-23 12:37:13 -0700

    Updated sync folder that has moved

--- scripts/processmymediafoldersforarchive.sh
+++ scripts/processmymediafoldersforarchive.sh
@@ -11,5 +11,6 @@ echo "BASEFOLDER is $BASEFOLDER"
 
 processmediafolderforarchive.sh "$BASEFOLDER/SyncthingChrisAndroid/AndroidCamera" "$BASEFOLDER/SyncthingProcessed/ChrisMedia/AndroidCamera"
 processmediafolderforarchive.sh "$BASEFOLDER/SyncthingChrisAndroid/PicturesFromApps" "$BASEFOLDER/SyncthingProcessed/ChrisMedia/PicturesFromApps"
-processmediafolderforarchive.sh "$BASEFOLDER/Syncthing/CodiDox" "$BASEFOLDER/SyncthingProcessed/CodiMedia"
+processmediafolderforarchive.sh "$BASEFOLDER/Syncthing/CodiDoxBackupSync" "$BASEFOLDER/SyncthingProcessed/CodiMedia"
 processmediafolderforarchive.sh "$BASEFOLDER/Syncthing/GooglePixel4aPhotos" "$BASEFOLDER/SyncthingProcessed/GooglePixel4aMedia"
+    
