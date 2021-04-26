53bbb1fc0336430d065f6aaa3f63243ae3c7e7ed
Chris Paiano <chris.paiano@gmail.com>
2021-04-25 18:28:16 -0700

    Output format of videoshowsize changed

--- scripts/videoshowsize.sh
+++ scripts/videoshowsize.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "$1"
+#Below formats to 1280x720 for example:
+#ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of csv=s=x:p=0 "$1"
+
+#Below formats to two lines, width=1280 and height=720 for example
+ffprobe -v error -select_streams v:0 -show_entries stream=width,height -of default=nw=1 "$1"
