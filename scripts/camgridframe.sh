d93c53ac4f69f9541889dc97a89bc5e5d10841e5
Chris Paiano <chris.paiano@gmail.com>
2021-05-20 00:52:40 -0700

    Moved to fim, wrote camgridloop.sh since service doesn't appear to be capable of properly displaying imagery

--- scripts/camgridframe.sh
+++ scripts/camgridframe.sh
@@ -3,4 +3,7 @@
 ffmpeg -y -loglevel fatal -rtsp_transport tcp -i "rtsp://sphene:mp3sheet@192.168.100.139/live" -frames:v 2 -r 1 -s 640x480 /ramdisk/sphene.jpg
 ffmpeg -y -loglevel fatal -rtsp_transport tcp -i "rtsp://rupee:mp3sheet@192.168.100.142/live" -frames:v 2 -r 1 -s 640x480 /ramdisk/rupee.jpg
 montage -label %f -background '#336699' -geometry +4+4 /ramdisk/sphene.jpg /ramdisk/rupee.jpg /ramdisk/camgrid.jpg
-fbi -T 1 -fitwidth -blend 500 /ramdisk/camgrid.jpg
+#fbi -T 1 -fitwidth -blend 500 /ramdisk/camgrid.jpg
+sudo killall fim
+/usr/bin/fim -T 1 --autowidth /ramdisk/camgrid.jpg &
+
