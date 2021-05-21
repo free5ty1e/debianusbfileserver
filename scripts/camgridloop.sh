62c705e3f23d94009af2aef9f1323601e2e49aaf
Chris Paiano <chris.paiano@gmail.com>
2021-05-21 13:03:43 -0700

    camgrid service now runs as pi user

--- scripts/camgridloop.sh
+++ scripts/camgridloop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "User is `id -nu`"
-if [[ `id -nu` != "pi" ]];then
-   echo "Not pi user, exiting.."
-   exit 1
-fi
+echo "User is `id -nu`, should be pi for this to work..."
+# if [[ `id -nu` != "pi" ]];then
+#    echo "Not pi user, exiting.."
+#    exit 1
+# fi
 
 screensaverdisable.sh
 # while [ 1 ]
