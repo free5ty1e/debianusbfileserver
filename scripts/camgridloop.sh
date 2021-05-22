0cfd83229355388cda97265c8d64a2acf4e69cf8
Chris Paiano <chris.paiano@gmail.com>
2021-05-22 10:58:33 -0700

    updated camgridloop.sh

--- scripts/camgridloop.sh
+++ scripts/camgridloop.sh
@@ -5,13 +5,16 @@ echo "User is `id -nu`, should be pi for this to work..."
 #    echo "Not pi user, exiting.."
 #    exit 1
 # fi
-
 screensaverdisable.sh
-# while [ 1 ]
-for i in {0..10}
+camgridstopframecaptures.sh
+camgridstartframecaptures.sh
+while [ 1 ]
+# for i in {0..10}
 do
-	echo "Generating frame $i of 10..."
-	camgridframe.sh
-   	sleep 1
+	# echo "Generating frame $i of 10..."
+	# camgridframe.sh
+	camgridgenerateframe.sh
+	camgriddisplayframe.sh
+   	sleep 5
    # sudo killall fim
 done
