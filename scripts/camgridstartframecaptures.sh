8e668963d0ab6d3b2a0eb680894a4fc4babfe09f
Chris Paiano <chris.paiano@gmail.com>
2021-05-22 10:54:33 -0700

    split new camgrid scripts out

---
+++ scripts/camgridstartframecaptures.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Timeout is in microseconds, so 5,000,000 = 5 seconds
+RTSP_TIMEOUT=5000000
+CAPTURE_RESOLUTION="640x480"
+
+ffmpeg -loglevel fatal -stimeout $RTSP_TIMEOUT -i "rtsp://sphene:mp3sheet@192.168.100.139/live" -vf fps=fps=1/5 -update 1 -an -y -s $CAPTURE_RESOLUTION /ramdisk/sphene.jpg </dev/null &
+export CAPTURE_SPHENE_PID=$!
+
+ffmpeg -loglevel fatal -stimeout $RTSP_TIMEOUT -i "rtsp://rupee:mp3sheet@192.168.100.142/live" -vf fps=fps=1/5 -update 1 -an -y -s $CAPTURE_RESOLUTION /ramdisk/rupee.jpg </dev/null &
+export CAPTURE_RUPEE_PID=$!
