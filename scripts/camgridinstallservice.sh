0fedf14b5bc8bc92cb8505443c7309314bb40a6c
Chris Paiano <chris.paiano@gmail.com>
2021-05-22 15:24:51 -0700

    Created xsession.target triggered by xsession to kick off camgrid service

--- scripts/camgridinstallservice.sh
+++ scripts/camgridinstallservice.sh
@@ -3,6 +3,8 @@
 echo "Installing / enabling camgrid.service and reloading systemctl daemon..."
 # sudo service camgrid stop
 sudo systemctl --user stop camgrid
+cp -vf debianusbfileserver/reference/xsession /home/pi/.xsession
+cp -vf debianusbfileserver/reference/xsession.target /home/pi/
 sudo cp -vf debianusbfileserver/reference/camgrid.service /etc/systemd/user/
 systemctl --user daemon-reload
 systemctl --user enable camgrid
