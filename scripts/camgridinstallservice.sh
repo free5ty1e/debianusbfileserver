9462a40d23f2ff8beedb65cd06f49cfca823c5b0
Chris Paiano <chris.paiano@gmail.com>
2021-05-22 14:34:47 -0700

    Now stopping service before reinstalling

--- scripts/camgridinstallservice.sh
+++ scripts/camgridinstallservice.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "Installing camgrid.service and reloading systemctl daemon..."
+sudo service camgrid stop
 sudo cp -vf debianusbfileserver/reference/camgrid.service /etc/systemd/system/
 sudo systemctl daemon-reload
