c2da9f3a7c44c89b7db104c601fa84a727a93cdb
Chris Paiano <chris.paiano@gmail.com>
2021-05-22 14:10:30 -0700

    Updating xfce desktop background with camgrid

--- scripts/camgriddisplayframe.sh
+++ scripts/camgriddisplayframe.sh
@@ -9,4 +9,14 @@
 
 # sudo killall xloadimage
 killall xloadimage || true
-xloadimage -display :0 -fullscreen /ramdisk/camgrid.jpg &
+# xloadimage -display :0 -fullscreen /ramdisk/camgrid.jpg &
+# xloadimage -display :0 -fullscreen /ramdisk/sphene.jpg /ramdisk/rupee.jpg &
+
+# montage -label %f -background '#336699' -geometry +4+4 /ramdisk/sphene.jpg /ramdisk/rupee.jpg jpg:- | display -display :0 -foreground "#000000" -backdrop -window root jpg:-
+#display -display :0 -foreground "#000000" -backdrop -window root jpg:-
+
+# montage -label %f -background '#336699' -geometry +4+4 /ramdisk/sphene.jpg /ramdisk/rupee.jpg jpg:- | display -display :0 -backdrop -window root jpg:- &
+
+#Updating wallpaper
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/image-style -s 4
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/workspace0/last-image -s /ramdisk/camgrid.jpg
