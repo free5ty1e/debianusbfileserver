b0551ad88a3dbdc6bbdf5459fe3a978ceabd8876
Chris Paiano <chris.paiano@gmail.com>
2021-05-23 15:50:34 -0700

    Now using a camgrid a / camgrid b method to seamlessly change desktop wallpaper without the flashing blank / background between frames

--- scripts/camgridgenerateframe.sh
+++ scripts/camgridgenerateframe.sh
@@ -4,11 +4,13 @@
 RTSP_TIMEOUT=5000000
 CAPTURE_RESOLUTION="640x480"
 
+echo "Generating camgrid frame to filename given as first parameter : $1"
+
 # ffmpeg -y -loglevel fatal -stimeout $RTSP_TIMEOUT -rtsp_transport tcp -i "rtsp://sphene:mp3sheet@192.168.100.139/live" -frames:v 2 -r 1 -s $CAPTURE_RESOLUTION /ramdisk/sphene.jpg
 # ffmpeg -y -loglevel fatal -rtsp_transport tcp -i "rtsp://sphene:mp3sheet@192.168.100.139/live" -frames:v 2 -r 1 -s 640x480 /ramdisk/sphene.bmp
 
 # ffmpeg -y -loglevel fatal -stimeout $RTSP_TIMEOUT -rtsp_transport tcp -i "rtsp://rupee:mp3sheet@192.168.100.142/live" -frames:v 2 -r 1 -s $CAPTURE_RESOLUTION /ramdisk/rupee.jpg
 # ffmpeg -y -loglevel fatal -rtsp_transport tcp -i "rtsp://rupee:mp3sheet@192.168.100.142/live" -frames:v 2 -r 1 -s 640x480 /ramdisk/rupee.bmp
 
-sudo montage -label %f -background '#336699' -geometry +4+4 /ramdisk/sphene.jpg /ramdisk/rupee.jpg /ramdisk/camgrid.jpg
+sudo montage -label %f -background '#336699' -geometry +4+4 /ramdisk/sphene.jpg /ramdisk/rupee.jpg "$1"
 # montage -label %f -background '#336699' -geometry +4+4 /ramdisk/sphene.bmp /ramdisk/rupee.bmp /ramdisk/camgrid.bmp
--- scripts/camgridloop.sh
+++ scripts/camgridloop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SECONDS_BETWEEN_DISPLAY_FRAMES=5
+
 echo "User is `id -nu`, should be pi for this to work..."
 # if [[ `id -nu` != "pi" ]];then
 #    echo "Not pi user, exiting.."
@@ -8,16 +10,20 @@ echo "User is `id -nu`, should be pi for this to work..."
 camgridstopframecaptures.sh
 camgridstartframecaptures.sh
 sleep 10
-camgridgenerateframe.sh
-camgridsetdesktopbackground.sh /ramdisk/camgrid.jpg
+camgridgenerateframe.sh /ramdisk/camgrid_a.jpg
+camgridsetdesktopbackground.sh /ramdisk/camgrid_a.jpg
 screensaverdisable.sh
 while [ 1 ]
 # for i in {0..10}
 do
 	# echo "Generating frame $i of 10..."
 	# camgridframe.sh
-	sleep 5
-	camgridgenerateframe.sh
+	sleep $SECONDS_BETWEEN_DISPLAY_FRAMES
+	camgridgenerateframe.sh /ramdisk/camgrid_b.jpg
+	camgridsetdesktopbackground.sh /ramdisk/camgrid_b.jpg
+	sleep $SECONDS_BETWEEN_DISPLAY_FRAMES
+	camgridgenerateframe.sh /ramdisk/camgrid_a.jpg
+	camgridsetdesktopbackground.sh /ramdisk/camgrid_a.jpg	
 	screensaverdisable.sh
 	# camgriddisplayframe.sh
    # sudo killall fim
