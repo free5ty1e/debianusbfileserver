420763649a93bce30294bca3cc52dc803f185947
Chris Paiano <chris.paiano@gmail.com>
2021-05-22 17:22:54 -0700

    .xsession -> .xsessionrc, now auto startup and auto login work together

--- scripts/camgridinstallservice.sh
+++ scripts/camgridinstallservice.sh
@@ -3,7 +3,7 @@
 echo "Installing / enabling camgrid.service and reloading systemctl daemon..."
 # sudo service camgrid stop
 systemctl --user stop camgrid
-cp -vf debianusbfileserver/reference/xsession /home/pi/.xsession
+cp -vf debianusbfileserver/reference/xsessionrc /home/pi/.xsessionrc
 chmod +x /home/pi/.xsession
 sudo cp -vf debianusbfileserver/reference/xsession.target /etc/systemd/user/
 sudo cp -vf debianusbfileserver/reference/camgrid.service /etc/systemd/user/
