7aff24399ef36a132d3f42d1d6dbc760d200dbda
Chris Paiano <chris.paiano@gmail.com>
2021-05-23 17:33:55 -0700

    Extracted stream 1 / 2 URL vars, genericized capture still filenames, extracted camgrid_geometry var

--- scripts/camgridgenerateframe.sh
+++ scripts/camgridgenerateframe.sh
@@ -3,6 +3,7 @@
 #Timeout is in microseconds, so 5,000,000 = 5 seconds
 RTSP_TIMEOUT=5000000
 CAPTURE_RESOLUTION="640x480"
+CAMGRID_GEOMETRY="+4+4"
 
 echo "Generating camgrid frame to filename given as first parameter : $1"
 
@@ -12,5 +13,5 @@ echo "Generating camgrid frame to filename given as first parameter : $1"
 # ffmpeg -y -loglevel fatal -stimeout $RTSP_TIMEOUT -rtsp_transport tcp -i "rtsp://rupee:mp3sheet@192.168.100.142/live" -frames:v 2 -r 1 -s $CAPTURE_RESOLUTION /ramdisk/rupee.jpg
 # ffmpeg -y -loglevel fatal -rtsp_transport tcp -i "rtsp://rupee:mp3sheet@192.168.100.142/live" -frames:v 2 -r 1 -s 640x480 /ramdisk/rupee.bmp
 
-sudo montage -label %f -background '#336699' -geometry +4+4 /ramdisk/sphene.jpg /ramdisk/rupee.jpg "$1"
+sudo montage -label %f -background '#336699' -geometry +4+4 /ramdisk/rtspstream1.jpg /ramdisk/rtspstream2.jpg "$1"
 # montage -label %f -background '#336699' -geometry +4+4 /ramdisk/sphene.bmp /ramdisk/rupee.bmp /ramdisk/camgrid.bmp
--- scripts/camgridstartframecaptures.sh
+++ scripts/camgridstartframecaptures.sh
@@ -4,9 +4,11 @@
 RTSP_TIMEOUT=5000000
 CAPTURE_RESOLUTION="640x480"
 CAPTURE_FPS="1/5"
+RTSP_STREAM_URL_1="rtsp://sphene:mp3sheet@192.168.100.139/live"
+RTSP_STREAM_URL_2="rtsp://rupee:mp3sheet@192.168.100.142/live"
 
-sudo ffmpeg -loglevel fatal -stimeout $RTSP_TIMEOUT -i "rtsp://sphene:mp3sheet@192.168.100.139/live" -vf fps=fps=$CAPTURE_FPS -update 1 -an -y -s $CAPTURE_RESOLUTION /ramdisk/sphene.jpg </dev/null &
-export CAPTURE_SPHENE_PID=$!
+sudo ffmpeg -loglevel fatal -stimeout $RTSP_TIMEOUT -i "$RTSP_STREAM_URL_1" -vf fps=fps=$CAPTURE_FPS -update 1 -an -y -s $CAPTURE_RESOLUTION /ramdisk/rtspstream1.jpg </dev/null &
+export CAPTURE_STREAM_PID_1=$!
 
-sudo ffmpeg -loglevel fatal -stimeout $RTSP_TIMEOUT -i "rtsp://rupee:mp3sheet@192.168.100.142/live" -vf fps=fps=$CAPTURE_FPS -update 1 -an -y -s $CAPTURE_RESOLUTION /ramdisk/rupee.jpg </dev/null &
-export CAPTURE_RUPEE_PID=$!
+sudo ffmpeg -loglevel fatal -stimeout $RTSP_TIMEOUT -i "$RTSP_STREAM_URL_2" -vf fps=fps=$CAPTURE_FPS -update 1 -an -y -s $CAPTURE_RESOLUTION /ramdisk/rtspstream2.jpg </dev/null &
+export CAPTURE_STREAM_PID_2=$!
