befd2e5804fc8d7d05f5ce1d4e7832c2bdcb3cdb
Chris Paiano <chris.paiano@gmail.com>
2021-05-23 17:27:43 -0700

    updated chmod to xsessionrc

--- scripts/camgridinstallservice.sh
+++ scripts/camgridinstallservice.sh
@@ -4,7 +4,7 @@ echo "Installing / enabling camgrid.service and reloading systemctl daemon..."
 # sudo service camgrid stop
 systemctl --user stop camgrid
 cp -vf debianusbfileserver/reference/xsessionrc /home/pi/.xsessionrc
-chmod +x /home/pi/.xsession
+chmod +x /home/pi/.xsessionrc
 sudo cp -vf debianusbfileserver/reference/xsession.target /etc/systemd/user/
 sudo cp -vf debianusbfileserver/reference/camgrid.service /etc/systemd/user/
 systemctl --user daemon-reload
