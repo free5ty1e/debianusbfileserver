171ca5f5279f910b5f45e9a003c676affa4a0794
Chris Paiano <chris.paiano@gmail.com>
2021-05-23 22:51:09 -0700

    conf file now should support array of stream urls and titles and auto handle camgrid'din them

--- scripts/camgridgenerateframe.sh
+++ scripts/camgridgenerateframe.sh
@@ -9,5 +9,16 @@ echo "Generating camgrid frame to filename given as first parameter : $1"
 # ffmpeg -y -loglevel fatal -stimeout $RTSP_TIMEOUT -rtsp_transport tcp -i "rtsp://rupee:mp3sheet@192.168.100.142/live" -frames:v 2 -r 1 -s $CAPTURE_RESOLUTION /ramdisk/rupee.jpg
 # ffmpeg -y -loglevel fatal -rtsp_transport tcp -i "rtsp://rupee:mp3sheet@192.168.100.142/live" -frames:v 2 -r 1 -s 640x480 /ramdisk/rupee.bmp
 
-sudo montage -label %f -background "$CAMGRID_BACKGROUND" -geometry "$CAMGRID_GEOMETRY" /ramdisk/rtspstream1.jpg /ramdisk/rtspstream2.jpg "$1"
+CAM_FILE_PARAMETER_LIST=()
+
+for i in "${!RTSP_STREAM_URLS[@]}"; do 
+	STREAM_URL=${RTSP_STREAM_URLS[$i]}
+	STREAM_TITLE=${RTSP_STREAM_TITLES[$i]}
+	echo "======----->>>Adding cam capture file $CAPTURE_LOCATION/$STREAM_TITLE.jpg to list of cams to add to camgrid..."
+	CAM_FILE_PARAMETER_LIST+=("$CAPTURE_LOCATION/$STREAM_TITLE.jpg")
+done
+echo "Constructing camgrid with cam files from array: ${CAM_FILE_PARAMETER_LIST[@]}"
+sudo montage -label %f -background "$CAMGRID_BACKGROUND" -geometry "$CAMGRID_GEOMETRY" ${CAM_FILE_PARAMETER_LIST[@]} "$1"
+
+# sudo montage -label %f -background "$CAMGRID_BACKGROUND" -geometry "$CAMGRID_GEOMETRY" /ramdisk/rtspstream1.jpg /ramdisk/rtspstream2.jpg "$1"
 # montage -label %f -background '#336699' -geometry +4+4 /ramdisk/sphene.bmp /ramdisk/rupee.bmp /ramdisk/camgrid.bmp
--- scripts/camgridstartframecaptures.sh
+++ scripts/camgridstartframecaptures.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 . $HOME/.camgrid/camgrid.conf
 
-sudo ffmpeg -loglevel fatal -stimeout $RTSP_TIMEOUT -i "$RTSP_STREAM_URL_1" -vf fps=fps=$CAPTURE_FPS -update 1 -an -y -s $CAPTURE_RESOLUTION /ramdisk/rtspstream1.jpg </dev/null &
-export CAPTURE_STREAM_PID_1=$!
+for i in "${!RTSP_STREAM_URLS[@]}"; do 
+	STREAM_URL=${RTSP_STREAM_URLS[$i]}
+	STREAM_TITLE=${RTSP_STREAM_TITLES[$i]}
+	echo "======----->>>Starting RTSP stream (# $i named $STREAM_TITLE) capture of URL $STREAM_URL at $CAPTURE_RESOLUTION $CAPTURE_FPS FPS to $CAPTURE_LOCATION/$STREAM_TITLE.jpg..."
+	sudo ffmpeg -loglevel fatal -stimeout $RTSP_TIMEOUT -i "$STREAM_URL" -vf fps=fps=$CAPTURE_FPS -update 1 -an -y -s $CAPTURE_RESOLUTION "$CAPTURE_LOCATION/$STREAM_TITLE.jpg" </dev/null &
+	export CAPTURE_STREAM_PID=$!
+done
 
-sudo ffmpeg -loglevel fatal -stimeout $RTSP_TIMEOUT -i "$RTSP_STREAM_URL_2" -vf fps=fps=$CAPTURE_FPS -update 1 -an -y -s $CAPTURE_RESOLUTION /ramdisk/rtspstream2.jpg </dev/null &
-export CAPTURE_STREAM_PID_2=$!
+# sudo ffmpeg -loglevel fatal -stimeout $RTSP_TIMEOUT -i "$RTSP_STREAM_URL_2" -vf fps=fps=$CAPTURE_FPS -update 1 -an -y -s $CAPTURE_RESOLUTION /ramdisk/rtspstream2.jpg </dev/null &
+# export CAPTURE_STREAM_PID_2=$!
+
