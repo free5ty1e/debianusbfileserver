21cffd52720954217bc900e7a08f20218fb13673
Chris Paiano <chris.paiano@gmail.com>
2021-05-25 23:11:41 -0700

    Trying fifo format with auto reconnect

--- scripts/camgridstartframecaptures.sh
+++ scripts/camgridstartframecaptures.sh
@@ -5,8 +5,8 @@ for i in "${!RTSP_STREAM_URLS[@]}"; do
 	STREAM_URL=${RTSP_STREAM_URLS[$i]}
 	STREAM_TITLE=${RTSP_STREAM_TITLES[$i]}
 	echo "======----->>>Starting RTSP stream (# $i named $STREAM_TITLE) capture of URL $STREAM_URL at $CAPTURE_RESOLUTION $CAPTURE_FPS FPS to $CAPTURE_LOCATION/$STREAM_TITLE.jpg..."
-	sudo ffmpeg -loglevel fatal -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 2 -i "$STREAM_URL" -vf fps=fps=$CAPTURE_FPS -update 1 -an -y -s $CAPTURE_RESOLUTION "$CAPTURE_LOCATION/$STREAM_TITLE.jpg" </dev/null &
-	#-stimeout $RTSP_TIMEOUT
+	sudo ffmpeg -loglevel fatal -f fifo -fifo_format flv -drop_pkts_on_overflow 1 -attempt_recovery 1 -recover_any_error 1 -i "$STREAM_URL" -vf fps=fps=$CAPTURE_FPS -update 1 -an -y -s $CAPTURE_RESOLUTION "$CAPTURE_LOCATION/$STREAM_TITLE.jpg" </dev/null &
+	#-stimeout $RTSP_TIMEOUT -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 2
 	export CAPTURE_STREAM_PID=$!
 done
 
