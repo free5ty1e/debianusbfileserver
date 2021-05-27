b29870b7102e74d402a67d31b1177f8e25fa70a7
Chris Paiano <chris.paiano@gmail.com>
2021-05-26 23:27:08 -0700

    Migrated to inotifywait reactive event based camgrid / frame updates to show frames as they come in

--- scripts/camgridloop.sh
+++ scripts/camgridloop.sh
@@ -8,6 +8,7 @@ echo "User is `id -nu`, should be pi for this to work..."
 # fi
 CAMGRIDFILEA="/ramdisk/camgrid_a.$CAPTURE_FORMAT"
 CAMGRIDFILEB="/ramdisk/camgrid_b.$CAPTURE_FORMAT"
+WHICHCAMGRID="$CAMGRIDFILEA"
 echo "Camgrid filenames are $CAMGRIDFILEA and $CAMGRIDFILEB"
 
 camgridstopframecaptures.sh
@@ -16,42 +17,42 @@ sleep 10
 camgridgenerateframe.sh "$CAMGRIDFILEA"
 camgridsetdesktopbackground.sh "$CAMGRIDFILEA"
 screensaverdisable.sh
-while [ 1 ]
-# for i in {0..10}
-do
-	# echo "Generating frame $i of 10..."
-	# camgridframe.sh
-	sleep $SECONDS_BETWEEN_DISPLAY_FRAMES
-	camgridgenerateframe.sh "$CAMGRIDFILEB"
-	camgridsetdesktopbackground.sh "$CAMGRIDFILEB"
-	sleep $SECONDS_BETWEEN_DISPLAY_FRAMES
-	camgridgenerateframe.sh "$CAMGRIDFILEA"
-	camgridsetdesktopbackground.sh "$CAMGRIDFILEA"
-	screensaverdisable.sh
-	# camgriddisplayframe.sh
-   # sudo killall fim
-done
 
 
-
+##OLD LOOP:
+# while [ 1 ]
+# # for i in {0..10}
+# do
+# 	# echo "Generating frame $i of 10..."
+# 	# camgridframe.sh
+# 	sleep $SECONDS_BETWEEN_DISPLAY_FRAMES
+# 	camgridgenerateframe.sh "$CAMGRIDFILEB"
+# 	camgridsetdesktopbackground.sh "$CAMGRIDFILEB"
+# 	sleep $SECONDS_BETWEEN_DISPLAY_FRAMES
+# 	camgridgenerateframe.sh "$CAMGRIDFILEA"
+# 	camgridsetdesktopbackground.sh "$CAMGRIDFILEA"
+# 	screensaverdisable.sh
+# 	# camgriddisplayframe.sh
+#    # sudo killall fim
+# done
 
 
 ##NEW LOOP:
-
-# inotifywait --quiet --event close_write,moved_to,create --monitor /ramdisk |
-# while read -r directory events filename; do
-  
-# 	for i in "${!RTSP_STREAM_TITLES[@]}"; do 
-# 		STREAM_TITLE=${RTSP_STREAM_TITLES[$i]}
-# 		if [ "$filename" == "$STREAM_TITLE.jpg" ]; then
-    		
-
-# 			#TODO: Need script that can tell which camgrid_a or _b was last generated, and generate the other one each cycle here and set it as the desktop background:
-
-
-
-#   		fi
-
-# 	done
-
-# done
+inotifywait --quiet --event close_write,moved_to,create --monitor /ramdisk |
+while read -r directory events filename; do
+	for i in "${!RTSP_STREAM_TITLES[@]}"; do 
+		STREAM_TITLE=${RTSP_STREAM_TITLES[$i]}
+		if [ "$filename" == "$STREAM_TITLE.$CAPTURE_FORMAT" ]; then
+			if [ WHICHCAMGRID == "$CAMGRIDFILEA" ]; then
+				camgridgenerateframe.sh "$CAMGRIDFILEB"
+				camgridsetdesktopbackground.sh "$CAMGRIDFILEB"
+				WHICHCAMGRID="$CAMGRIDFILEB"
+			else
+				camgridgenerateframe.sh "$CAMGRIDFILEA"
+				camgridsetdesktopbackground.sh "$CAMGRIDFILEA"
+				screensaverdisable.sh
+				WHICHCAMGRID="$CAMGRIDFILEA"
+			fi
+  		fi
+	done
+done
