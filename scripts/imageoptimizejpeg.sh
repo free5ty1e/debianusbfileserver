d6de7e755f178f545cf1cb6b26ee0585ce01527f
Chris Paiano <chris.paiano@gmail.com>
2021-06-19 09:33:05 -0700

    Nice -20 for image processing too for background operations

--- scripts/imageoptimizejpeg.sh
+++ scripts/imageoptimizejpeg.sh
@@ -2,4 +2,4 @@
 
 echo "Optimizing JPEG image $1 to $2.  If needed, follow instructions here to install jpeg-recompress tool:"
 echo "https://github.com/free5ty1e/debianusbfileserver/blob/master/README.md"
-jpeg-recompress --quality medium --accurate --method smallfry --min 50 "$1" "$2"
+nice -20 jpeg-recompress --quality medium --accurate --method smallfry --min 50 "$1" "$2"
--- scripts/imageshrinktohd.sh
+++ scripts/imageshrinktohd.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Resizing image $1 to HD as $2.  If needed, sudo apt-get install imagemagick"
-convert "$1" -resize 1920x1920\> "$2"
+nice -20 convert "$1" -resize 1920x1920\> "$2"
