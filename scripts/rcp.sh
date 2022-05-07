55660ddf11dcde4e66920793cba4e3318e59736d
Chris Paiano <chris.paiano@gmail.com>
2022-05-07 10:52:39 -0700

    back to 2 parms with quotes

--- scripts/rcp.sh
+++ scripts/rcp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "rsync cp: rsync --verbose --stats --whole-file --progress --human-readable $1 $2 $3 $4 $5 $6 $7 $8 $9"
-rsync --verbose --stats --whole-file --progress --human-readable $1 $2 $3 $4 $5 $6 $7 $8 $9
+echo "rsync cp: rsync --verbose --stats --whole-file --progress --human-readable \"$1\" \"$2\""
+rsync --verbose --stats --whole-file --progress --human-readable "$1" "$2"
