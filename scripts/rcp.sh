0e3e39ee7d1d75eccf6ec6bef449fd0e208d0d1b
Chris Paiano <chris.paiano@gmail.com>
2022-05-07 10:48:59 -0700

    cleanup

--- scripts/rcp.sh
+++ scripts/rcp.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-message="Initiating a drive speed test for $1 (you did provide a drive device name such as /dev/sda, didn't you?) ..."
-echo "$message"
-cowsay -f eyes "$message"
-
 echo "rsync cp: rsync --verbose --stats --whole-file --progress --human-readable $1 $2 $3 $4 $5 $6 $7 $8 $9"
-
 rsync --verbose --stats --whole-file --progress --human-readable $1 $2 $3 $4 $5 $6 $7 $8 $9
