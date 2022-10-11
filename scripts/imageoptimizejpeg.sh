78766be54a689385b559d4c6205fc30eae3ab8f8
Chris Paiano <chris.paiano@gmail.com>
2022-10-10 17:45:10 -0700

    Migrated cphotos scripts out to cphotos repo

--- scripts/imageoptimizejpeg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Optimizing JPEG image $1 to $2.  If needed, follow instructions here to install jpeg-recompress tool:"
-echo "https://github.com/free5ty1e/debianusbfileserver/blob/master/README.md"
-nice -20 jpeg-recompress --quality medium --accurate --method smallfry --min 50 "$1" "$2"
--- scripts/videoshowheight.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ffprobe -v error -select_streams v:0 -show_entries stream=height -of default=nw=1:nk=1 "$1"
