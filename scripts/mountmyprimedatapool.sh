1e6d0f10e4cde61ec1bd7828146b49b8bfdf5940
Chris Paiano <chris.paiano@gmail.com>
2022-11-08 23:30:42 -0800

    Even less options for primedatapool mount?

--- scripts/mountmyprimedatapool.sh
+++ scripts/mountmyprimedatapool.sh
@@ -13,4 +13,4 @@ echo "You should have added this computer's SSH public key to your fileserver fo
 
 mkdir -vp /media/PrimeDataPool
 # mount -t cifs -o user=pi,username=pi,uid=$(id -u),gid=$(id -g),forceuid,forcegid //192.168.100.105/PrimeDataPool /media/PrimeDataPool
-mount -t cifs -o user=pi,username=pi,uid=$(id -u),gid=$(id -g) //192.168.100.105/PrimeDataPool /media/PrimeDataPool
+mount -t cifs -o user=pi,username=pi //192.168.100.105/PrimeDataPool /media/PrimeDataPool
