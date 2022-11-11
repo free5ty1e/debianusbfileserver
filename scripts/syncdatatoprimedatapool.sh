cb61fe3a92a190d3f43ff7104d926d961fb0487a
Chris Paiano <chris.paiano@gmail.com>
2022-11-10 18:36:46 -0800

    confirmed dryrun was ok to try

--- scripts/syncdatatoprimedatapool.sh
+++ scripts/syncdatatoprimedatapool.sh
@@ -3,4 +3,4 @@
 echo "Customize a copy of this script to sync your own fileserver drive names"
 
 echo "Syncing data drive"
-syncremotedrive.sh /media/MassiveSto8TbAyy /media/PrimeDataPool/FileSrvData dryrun Syncthing SyncthingChrisAndroid
+syncremotedrive.sh /media/MassiveSto8TbAyy /media/PrimeDataPool/FileSrvData nodryrun Syncthing SyncthingChrisAndroid
